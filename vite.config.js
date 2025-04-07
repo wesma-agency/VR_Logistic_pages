aa5f23f59ecbd3e047133f382f84a04322ebb11c
Mihail Irt <shegaasher@gmail.com>
2025-04-07 11:08:58 +0300

    инициализация гита

---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite';
+
+import path from "path";
+import injectHTML from 'vite-plugin-html-inject';
+import postcssImport from 'postcss-import';
+import { resolve } from 'path';
+
+export default defineConfig({
+	base: './',
+
+	plugins: [injectHTML()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				// additionalData: `@import "${resolve(__dirname, 'src/styles/_variables.scss')}";
+				// 				@import "${resolve(__dirname, 'src/styles/_mixins.scss')}";`,
+			},
+		},
+		postcss: {
+		  plugins: [
+			postcssImport(),
+		  ],
+		},
+	},
+	server: {
+		// open: true,
+	},
+	root: './',
+	build: {
+		rollupOptions: {
+			input: {
+				index: 'index.html',
+			},
+		},
+		// Не обязательные опции
+		minify: false,
+	},
+	resolve: {
+		alias: {
+		  "@": path.resolve(__dirname, "./src/"),
+		  public: `${path.resolve(__dirname, "./public/")}`,
+		  pages: path.resolve(__dirname, "./src/html"),
+		},
+	},
+	preview: {
+
+	},
+});
